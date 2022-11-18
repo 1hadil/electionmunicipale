164887422d7729dc9dd1fdd310f67710bbd2c49a
aymeennefzi <aymen.nefzi@esprit.tn>
2022-11-18 16:13:19 +0100

    Ajout

---
+++ GladeAF/Interface/src/callbacks.c
@@ -0,0 +1,72 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_entry2_insert_text                  (GtkEditable     *editable,
+                                        gchar           *new_text,
+                                        gint             new_text_length,
+                                        gpointer         position,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_entry3_insert_text                  (GtkEditable     *editable,
+                                        gchar           *new_text,
+                                        gint             new_text_length,
+                                        gpointer         position,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_buttonConnecter_clicked             (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_button2_clicked                     (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_buttonGE_clicked                    (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_buttonGBV_clicked                   (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_buttonsupp_clicked                  (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
---
+++ GladeAF/Interface/src/callbacks.h
@@ -0,0 +1,36 @@
+#include <gtk/gtk.h>
+
+
+void
+on_entry2_insert_text                  (GtkEditable     *editable,
+                                        gchar           *new_text,
+                                        gint             new_text_length,
+                                        gpointer         position,
+                                        gpointer         user_data);
+
+void
+on_entry3_insert_text                  (GtkEditable     *editable,
+                                        gchar           *new_text,
+                                        gint             new_text_length,
+                                        gpointer         position,
+                                        gpointer         user_data);
+
+void
+on_buttonConnecter_clicked             (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button2_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonGE_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonGBV_clicked                   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonsupp_clicked                  (GtkButton       *button,
+                                        gpointer         user_data);
